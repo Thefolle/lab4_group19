791f953a3a90cde9352b20ff46300b1332f6e029
Davide Calarco <s280174@studenti.polito.it>
2021-05-03 11:52:10 +0200

    Initial commit

---
+++ src/main/kotlin/it/polito/group19/lab4/Lab4Application.kt
@@ -0,0 +1,11 @@
+package it.polito.group19.lab4
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Lab4Application
+
+fun main(args: Array<String>) {
+    runApplication<Lab4Application>(*args)
+}
---
+++ src/test/kotlin/it/polito/group19/lab4/Lab4ApplicationTests.kt
@@ -0,0 +1,13 @@
+package it.polito.group19.lab4
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Lab4ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
